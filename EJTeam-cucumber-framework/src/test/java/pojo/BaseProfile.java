97a0ec94c33e8f848441307fe8826d8a76a9ec8d
Jack Vu <jacknvu98@gmail.com>
2023-03-29 16:16:33 -0400

    adding base profile still need to missing education and experience waiting for pojo of these

---
+++ EJTeam-cucumber-framework/src/test/java/pojo/BaseProfile.java
@@ -0,0 +1,17 @@
+package pojo;
+
+public class BaseProfile {
+	
+	public String status;
+	public String company;
+	public String website;
+	public String location;
+	public String[] skills;
+	public String githubUsername;
+	public String bio; 
+	public Social social; 
+	public String _id;
+	public String date;
+	public int __v; 
+
+}
---
+++ EJTeam-cucumber-framework/src/test/java/pojo/Social.java
@@ -0,0 +1,18 @@
+package pojo;
+
+public class Social {
+	
+	public String twitter;
+	public String facebook;
+	public String youtube;
+	public String indeed;
+	public String instagram; 
+	
+	public Social(String twitter, String facebook, String youtube, String indeed, String instagram) {
+		this.twitter = twitter;
+		this.facebook = facebook;
+		this.youtube = youtube;
+		this.indeed = indeed;
+		this.instagram = instagram;
+	}
+}
